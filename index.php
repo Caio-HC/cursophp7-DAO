d973fdf27b8a26a1168f860874228368d7bdad84
Caio Henrique Clemente <caiohenrique_-2@hotmail.com>
2017-04-15 11:47:50 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -2,7 +2,20 @@
 require_once("config.php");
 
 
-$root = new Usuario();
+/*$root = new Usuario();
 $root->loadById(1);
 echo $root;
+
+busca tudo
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+/* Faz a busca pelo login
+$search = Usuario::search("a");
+echo json_encode($search);
+*/
+//Carega via autentificação
+$usuario = new Usuario();
+$usuario->login("Caio", "123456");
+echo $usuario;
 ?>
